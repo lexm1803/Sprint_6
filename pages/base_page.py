58d8d3c07499832a7cb6a1321b1add49e7bc9446
lexm18 <aleksa9518@mail.ru>
2025-12-04 23:47:33 +0300

    created main_page obj

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,4 @@
 import allure
-from typing import Tuple
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,19 +8,11 @@ from selenium.common.exceptions import TimeoutException
 
 class BasePage:
 
-    '''
-    Базовый класс для всех страниц, 
-    содержит общие методы взаимодействия с элементами и
-    интеграцию с Allure.
-    '''
-    def __init__(self, driver: WebDriver, timeout: int = 10):
+    def __init__(self, driver, timeout = 10):
         self.driver = driver
         self.wait = WebDriverWait(driver, timeout, poll_frequency = 1)
 
-    def _attache_screenshot_on_failer(self, step_name: str):
-        '''
-        Делает скриншоты и прикрепляет их к отчету в случае ошибки.
-        '''
+    def _attache_screenshot_on_failer(self, step_name):
         try:
             screenshot = self.driver.get_screenshot_as_png()
             allure.attach(
@@ -34,11 +25,7 @@ class BasePage:
             print(f'screenshot с ошибкой {e} прикреплен к отчету')
 
     @allure.step('Найти элемент по локатору {locator}')
-    def find_element(self, locator: Tuple[str, str]):
-        '''
-        Ищет элемент на странице и возращает его после появления в DOM.
-        Используется для проверки атрибута.
-        '''
+    def find_element(self, locator):
         try:
             return self.wait.until(EC.presence_of_element_located(locator))
         
@@ -47,11 +34,7 @@ class BasePage:
             raise
     
     @allure.step('Найти видимый элемент по локатору {locator}')
-    def find_visible_element(self, locator: Tuple[str, str]):
-        '''
-        Ищет элемент и ждет, пока он станет видимым.
-        '''
-
+    def find_visible_element(self, locator):
         try:
             return self.wait.until(EC.visibility_of_element_located(locator))
         
@@ -60,11 +43,7 @@ class BasePage:
             raise
     
     @allure.step('Найти список элементов по локатору {locator}')
-    def find_elements(self, locator: Tuple[str, str]):
-        '''
-        Ищет список элементов на странице и возращает их после появления в DOM.
-        Используется для проверки атрибутов.
-        '''
+    def find_elements(self, locator):
         try:
             return self.wait.until(EC.presence_of_elements_located(locator))
         
@@ -73,10 +52,7 @@ class BasePage:
             raise
     
     @allure.step('Клик по элементу {locator}')
-    def click(self, locator: Tuple[str, str]):
-        '''
-        Клик по элементу после ожидания кликабельности
-        '''
+    def click(self, locator):
         try:
             self.wait.until(EC.element_to_be_clickable(locator)).click()
         
@@ -85,10 +61,7 @@ class BasePage:
             raise
     
     @allure.step('Ввод текста "{text}" в поле {locator}')
-    def send_keys(self, locator: Tuple[str, str], text: str):
-        '''
-        Ищет видимый элемент, очищает, вводит `text`
-        '''
+    def send_keys(self, locator, text):
         try:
            element = self.find_visible_element(locator)
            element.clear()
@@ -99,11 +72,7 @@ class BasePage:
             raise
     
     @allure.step('Получить текст элемента {locator}')
-    def get_text(self, locator: Tuple[str, str]):
-        '''
-        Проверяет видимость элемента и возвращает 
-        текст видимого элемента
-        '''
+    def get_text(self, locator):
         try:
             return self.find_visible_element(locator).text
         
@@ -112,10 +81,7 @@ class BasePage:
             raise
     
     @allure.step('Проверить видимость элемента {locator}')
-    def is_element_visible(self, locator: Tuple[str, str]):
-        '''
-        Проверяет видимый ли элемент, без исключения при отсутствии
-        '''
+    def is_element_visible(self, locator):
         try:
             self.find_visible_element(locator)
             return True
@@ -128,10 +94,7 @@ class BasePage:
             raise
 
     @allure.step('Скролл до элемента {locator}')
-    def scroll_to_element(self, locator: Tuple[str,str]):
-        '''
-        Скролл страницы до элемента вне viewport
-        '''
+    def scroll_to_element(self, locator):
         try:
             element = self.find_element(locator)
             self.driver.execut_script("arguments[0].scrollIntoView({block: 'center'});", element)
---
+++ pages/main_page.py
@@ -0,0 +1,133 @@
+import allure
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class MainPage(BasePage):
+    MAIN_TITLE = (By.XPATH, '//div[@class="Home_Header__iJKdX"]')
+    MAIN_DESCRIPTION = (By.XPATH, '//div[@class="Home_Header__iJKdX"]/div[@class="Home_SubHeader__zwi_E"]')
+
+    BUTTON_ORDER_IN_THE_HEADER = (By.XPATH, "//div[@class='Header_Nav__AGCXC']//button[text()='Заказать']")
+    BUTTON_ORDER_ON_THE_PAGE = (By.XPATH, "//div[contains(@class, 'FinishButton')]//button[text()='Заказать']")
+
+    LOGO_SCOOTER = (By.XPATH, "//a[@href='/']//img[@alt='Scooter']")
+    LOGO_YANDEX = (By.XPATH, "//a[@href='//yandex.ru']//img[@alt='Yandex']")
+
+    HOW_IT_WORKS_TITLE = (By.XPATH, "//div[text()='Как это работает']")
+    FIRST_STEP_TITLE = (By.XPATH, "//div[text()='Заказываете самокат']")
+    FIRST_STEP_DESC = (By.XPATH, "//div[text()='Выбираете, когда и куда привезти']")
+
+    FAQ_SECTION_TITLE = (By.XPATH, "//div[text()='Вопросы о важном']")
+    FAQ_CONTAINER = (By.XPATH, "//div[@data-accordion-component='Accordion']")
+    FAQ_ITEM = (By.XPATH, ".//div[@data-accordion-component='AccordionItem']")
+    FAQ_BUTTON = (By.XPATH, ".//div[@data-accordion-component='AccordionItemButton']")
+    FAQ_PANEL = (By.XPATH, ".//div[@data-accordion-component='AccordionItemPanel']")
+
+    COOKIE_BANNER = (By.CLASS_NAME, "App_CookieConsent__1yUIN")
+    COOKIE_ACCEPT_BUTTON = (By.ID, "rcc-confirm-button")
+
+    TRACK_INPUT_HEADER = (By.XPATH, "//input[@placeholder='Введите номер заказа']")
+    TRACK_BUTTON_HEADER = (By.XPATH, "//button[text()='Go!']")
+    STATUS_BUTTON = (By.XPATH, "//button[text()='Статус заказа']")
+
+    URL = 'https://qa-scooter.praktikum-services.ru/'
+
+    # Перенести в base
+    @allure.step('Открыть страницу {self.URL}')
+    def open(self):
+        self.driver.get(self.URL)
+        self.accept_cookies_if_present()
+
+    @allure.step('Принять cookie, если баннер отображен')
+    def accept_cookies_if_present(self):
+        try:
+            if self.is_element_visible(self.COOKIE_BANNER):
+                self.click(self.COOKIE_ACCEPT_BUTTON)
+                #TO DO: явное ожидание перенести в base?
+                self.wait.until(EC.invisibility_of_element_located(self.COOKIE_BANNER))
+        except Exception:
+            pass
+    
+    @allure.step('Проверка, что страница загружена')
+    def is_loaded(self):
+        try:
+            return self.is_element_visible(self.MAIN_TITLE)
+        except Exception:
+            return False
+
+    @allure.step('Клик по кнопке "Заказать" в хэдере')
+    def click_order_button_top(self):
+        self.click(self.BUTTON_ORDER_IN_THE_HEADER)
+
+    @allure.step('Клик по кнопке "Заказать" на странице')
+    def click_order_button_top(self):
+        self.scroll_to_element(self.BUTTON_ORDER_ON_THE_PAGE)
+        self.click(self.BUTTON_ORDER_ON_THE_PAGE)
+
+    @allure.step('Нажать на логотип самоката')
+    def click_logo_scooter(self):
+        self.click(self.LOGO_SCOOTER)
+
+    @allure.step('Нажать на логотип Яндекс')
+    def click_logo_yandex(self):
+        self.click(self.LOGO_YANDEX)
+
+    @allure.step('Получение списка количества вопросов в блоке FAQ')
+    def get_faq_count(self):
+        conteiner = self.find_element(self.FAQ_CONTAINER)
+        items = conteiner.find_elements(*self.FAQ_ITEM)
+        return len(items)
+    
+    @allure.step('Получение текста вопроса №{index}')
+    def get_faq_question_text(self, index):
+        conteiner = self.find_element(self.FAQ_CONTAINER)
+        items = conteiner.find_elements(*self.FAQ_ITEM)
+        if index >= len(items):
+            raise IndexError(f'Индекс {index} выходит за пределы списка вопросов {len(items)}')
+        button = items[index].find_element(*self.FAQ_BUTTON)
+        return button.text.strip()
+    
+    @allure.step('Раскрыть вопрос №{index}')
+    def click_faq_question(self, index):
+        conteiner = self.find_element(self.FAQ_CONTAINER)
+        items = conteiner.find_elements(*self.FAQ_ITEM)
+        if index >= len(items):
+            raise IndexError(f'Индекс {index} выходит за пределы списка вопросов {len(items)}')
+        button = items[index].find_element(*self.FAQ_BUTTON)
+        # TO DO: создать метод вставки JS в консоль в base?
+        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", button)
+        self.driver.execute_script(
+            "arguments[0].style.zIndex = '9999'; "
+            "arguments[0].style.position = 'relative';",
+            button
+        )
+        self.driver.execute_script("arguments[0].click();", button)
+
+    @allure.step('Получить текст ответа на вопрос №{index}')
+    def get_faq_answer_text(self, index):
+        conteiner = self.find_element(self.FAQ_CONTAINER)
+        items = conteiner.find_elements(*self.FAQ_ITEM)
+        if index >= len(items):
+            raise IndexError(f'Индекс {index} выходит за пределы списка вопросов {len(items)}')
+        panel = items[index].find_element(*self.FAQ_PANEL)
+        #TO DO: подумать над другой реализацией
+        self.wait.until(
+            lambda driver: panel.text.strip() != '', 
+            message = f'Ответ №{index} пуст'
+        )
+        return panel.text.strip()
+    
+    #TO DO: перенести в base?
+    @allure.step('Ввести номер заказа в хэдере: {track_id}')
+    def enter_track_id_in_header(self, track_id):
+        self.send_keys(self.TRACK_INPUT_HEADER, track_id)
+
+    @allure.step('Нажать на "Go" в хэдере')
+    def click_track_go_button(self):
+        self.click(self.TRACK_BUTTON_HEADER)
+
+    @allure.step('Нажать на кнопку "Статус заказа"')
+    def click_status_button(self):
+        self.click(self.STATUS_BUTTON)
+    
